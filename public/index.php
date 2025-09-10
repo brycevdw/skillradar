d2177f66888c26d1f28829c0eb87faae3e126e44
bryce <145542285+brycevdw@users.noreply.github.com>
2025-09-10 14:53:35 +0200

    update

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$dbHost = $_ENV['DB_HOST'];
+$dbName = $_ENV['DB_NAME'];
+$dbUser = $_ENV['DB_USER'];
+$dbPass = $_ENV['DB_PASS'];
